b7a3f6c161577063fa5f3b063c4c64840b70028a
Dan5433 <grishkov.daniil@gmail.com>
2026-09-22 19:23:36 -0400

    Rework parry timing

--- parry_orbs.gd
+++ parry_orbs.gd
@@ -1,36 +1,30 @@
 extends Area2D
 
-const PARRY_FRAMES = 20;
+const PARRY_ACTIVE_FRAMES = 4;
 
-@onready var label: Label = $"../CanvasLayer/Label"
+@onready var label_2: Label = $"../Player/Label"
 @onready var player: CharacterBody2D = $"../Player"
 
-var touching = false;
-var touching_frames = 0;
+var parry_frames = 0
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
 
-func _process(delta: float) -> void:
-	if(touching):
-		touching_frames += 1
-	else:
-		touching_frames = 0
-	label.text = str(touching_frames)
+func _process(delta:float) -> void:
+	if(Input.is_action_just_pressed("parry")):
+		parry_frames = PARRY_ACTIVE_FRAMES
 		
-	if(not touching or not Input.is_action_just_pressed("parry") or touching_frames > PARRY_FRAMES):
-		return
-		
-	player.jump()
+	label_2.text = str(parry_frames)
+	
+func _physics_process(delta: float) -> void:
+	if(parry_frames > 0):
+		parry_frames -= 1;
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if(body is not CharacterBody2D):
+	if(body != player):
 		return
-	touching = true
-	
-func _on_body_exited(body: Node2D) -> void:
-	if(body is not CharacterBody2D):
-		return
-	touching = false
+		
+	if(parry_frames > 0):
+		player.jump()
+		parry_frames = 0
