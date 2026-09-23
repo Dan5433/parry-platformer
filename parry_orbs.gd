cda24f7322054929f88e4d2cc625af4587ccab3b
Dan5433 <grishkov.daniil@gmail.com>
2026-09-23 12:05:27 -0400

    Add parry cooldown
    Prevent autoclickers and fast clicking

--- parry_orbs.gd
+++ parry_orbs.gd
@@ -1,30 +1,43 @@
 extends Area2D
 
 const PARRY_ACTIVE_FRAMES = 4;
+const PARRY_COOLDOWN_FRAMES = 20;
 
-@onready var label_2: Label = $"../Player/Label"
+@onready var label: Label = $"../Player/Label"
 @onready var player: CharacterBody2D = $"../Player"
 
 var parry_frames = 0
+var parry_cooldown = 0
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 
 func _process(delta:float) -> void:
 	if(Input.is_action_just_pressed("parry")):
-		parry_frames = PARRY_ACTIVE_FRAMES
+		if(parry_frames > 0):
+			parry_frames = 0
+			parry_cooldown = PARRY_COOLDOWN_FRAMES * 5
+		elif(not parry_cooldown > 0):
+			parry_frames = PARRY_ACTIVE_FRAMES
+			
 		
-	label_2.text = str(parry_frames)
+	label.text = str(parry_frames)
 	
 func _physics_process(delta: float) -> void:
 	if(parry_frames > 0):
 		parry_frames -= 1;
+	
+	if(parry_cooldown > 0):
+		parry_cooldown -= 1;
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if(body != player):
 		return
 		
-	if(parry_frames > 0):
-		player.jump()
-		parry_frames = 0
+	if(not parry_frames > 0):
+		return
+		
+	player.jump()
+	parry_frames = 0
+	parry_cooldown = PARRY_COOLDOWN_FRAMES
