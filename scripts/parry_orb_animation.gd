dcb3e43e006ad4198ccf98f059094eea822e2452
Dan5433 <grishkov.daniil@gmail.com>
2026-09-24 22:40:13 -0400

    Animate parry orb

---
+++ scripts/parry_orb_animation.gd
@@ -0,0 +1,21 @@
+extends Sprite2D
+
+
+@export var animation_speed: float = 1
+var anim_direction = 1
+var anim_timer = animation_speed;
+
+
+func _process(delta: float) -> void:
+	anim_timer -= delta
+	
+	if(anim_timer > 0):
+		return
+	
+	anim_timer = animation_speed
+	
+	frame += anim_direction
+	if frame == 0:
+		anim_direction = 1
+	if frame == hframes -1:
+		anim_direction = -1
